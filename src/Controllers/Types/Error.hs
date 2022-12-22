a814c315ba7f1a62f71e931f7e773097b1bf32d0
Nicolás Urquiola <nicourquiola@gmail.com>
2022-12-21 23:16:50 -0300

    Add custom error formatters

--- src/Controllers/Types/Error.hs
+++ src/Controllers/Types/Error.hs
@@ -3,13 +3,34 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Controllers.Types.Error
-  (Error (..), notFound, serverError, unauthorized, forbidden) where
+  ( Error(..)
+  , notFound
+  , serverError
+  , unauthorized
+  , forbidden
+  , customFormatters
+  , badRequest
+  ) where
 
 import qualified Data.Aeson as Aeson
 import Data.Aeson.Types (ToJSON)
-import Data.Text (Text)
+import Data.Text (Text, pack)
+import Data.Text.Encoding (decodeUtf8)
 import GHC.Generics (Generic)
-import Servant (ServerError(..), err401, err403, err404, err500)
+import Network.Wai (rawPathInfo)
+import qualified Servant as ErrorFormatters (ErrorFormatters(..))
+import Servant
+  ( ErrorFormatter
+  , ErrorFormatters
+  , NotFoundErrorFormatter
+  , ServerError(..)
+  , defaultErrorFormatters
+  , err400
+  , err401
+  , err403
+  , err404
+  , err500
+  )
 
 newtype Error = Error { error :: Text } deriving (Generic, ToJSON)
 
@@ -30,3 +51,21 @@ unauthorized = jsonContent err401
 
 forbidden :: ServerError
 forbidden = jsonContent err403
+
+badRequest :: Text ->  ServerError
+badRequest = jsonContent . withMessage err400
+
+notFoundErrorFormatter :: NotFoundErrorFormatter
+notFoundErrorFormatter req = notFound (path <> " not found")
+  where path = decodeUtf8 (rawPathInfo req)
+
+customErrorFormatter :: ErrorFormatter
+customErrorFormatter _ _ = badRequest . pack
+
+customFormatters :: ErrorFormatters
+customFormatters = defaultErrorFormatters
+  { ErrorFormatters.notFoundErrorFormatter = notFoundErrorFormatter
+  , ErrorFormatters.urlParseErrorFormatter = customErrorFormatter
+  , ErrorFormatters.bodyParserErrorFormatter = customErrorFormatter
+  , ErrorFormatters.headerParseErrorFormatter = customErrorFormatter
+  }
